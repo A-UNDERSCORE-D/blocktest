df5ab18252ea70961c1c61de7bf7b89af23c09dc
A_D <A_D@snoonet.org>
2018-10-15 13:56:49 +0200

    added ItemBlockToolTip, sets tooltips based on lang file descriptions

---
+++ src/main/kotlin/items/ItemBlockToolTip.kt
@@ -0,0 +1,16 @@
+package ad.blocktest.items
+
+import net.minecraft.block.Block
+import net.minecraft.client.util.ITooltipFlag
+import net.minecraft.item.ItemBlock
+import net.minecraft.item.ItemStack
+import net.minecraft.util.text.TextComponentTranslation
+import net.minecraft.world.World
+
+class ItemBlockToolTip(block: Block): ItemBlock(block) {
+    override fun addInformation(stack: ItemStack, worldIn: World?, tooltip: MutableList<String>, flagIn: ITooltipFlag) {
+        super.addInformation(stack, worldIn, tooltip, flagIn)
+        val desc: String = TextComponentTranslation(this.block.unlocalizedName + ".description").formattedText
+        tooltip.add(desc)
+    }
+}
