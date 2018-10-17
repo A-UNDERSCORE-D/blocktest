e712ecf2a07e6f7d5bc1b2eac1992857f1519b8f
A_D <A_D@snoonet.org>
2018-10-17 16:48:19 +0200

    set creative tab for testItem

--- src/main/kotlin/items/TestItem.kt
+++ src/main/kotlin/items/TestItem.kt
@@ -1,6 +1,7 @@
 package ad.blocktest.items
 
 import ad.blocktest.BlockTest
+import net.minecraft.creativetab.CreativeTabs
 import net.minecraft.item.Item
 import net.minecraft.util.ResourceLocation
 
@@ -8,5 +9,6 @@ object TestItem: Item() {
     init {
         unlocalizedName = BlockTest.modId + ".test_item"
         registryName = ResourceLocation(BlockTest.modId, "test_item")
+        creativeTab = CreativeTabs.MISC
     }
 }
