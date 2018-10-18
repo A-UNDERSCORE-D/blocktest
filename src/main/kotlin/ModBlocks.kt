89796f438bcc73ae0c99690eb185ff4ce3b34ad1
A_D <A_D@snoonet.org>
2018-10-18 04:07:26 +0200

    removed type annotations from blocks

--- src/main/kotlin/ModBlocks.kt
+++ src/main/kotlin/ModBlocks.kt
@@ -1,24 +1,22 @@
 package ad.blocktest
 
-import ad.blocktest.blocks.BlockRandPlace
-import net.minecraft.block.Block
 import net.minecraftforge.fml.common.registry.GameRegistry
 
 object ModBlocks {
     @GameRegistry.ObjectHolder("blocktest:test_block")
-    val blockTest: Block = ad.blocktest.blocks.TestBlock()
+    val blockTest = ad.blocktest.blocks.TestBlock()
 
     @GameRegistry.ObjectHolder("blocktest:rand_block")
-    val blockRandPlace: BlockRandPlace = ad.blocktest.blocks.BlockRandPlace
+    val blockRandPlace = ad.blocktest.blocks.BlockRandPlace
 
     @GameRegistry.ObjectHolder("blocktest:bang_block")
-    val blockBang: ad.blocktest.blocks.BlockBang = ad.blocktest.blocks.BlockBang
+    val blockBang = ad.blocktest.blocks.BlockBang
 
     @GameRegistry.ObjectHolder("blocktest:shape_block")
-    val blockShape: ad.blocktest.blocks.BlockShape = ad.blocktest.blocks.BlockShape
+    val blockShape = ad.blocktest.blocks.BlockShape
 
     @GameRegistry.ObjectHolder("blocktest:rain_block")
-    val blockStopRain: ad.blocktest.blocks.BlockStopRain = ad.blocktest.blocks.BlockStopRain()
+    val blockStopRain = ad.blocktest.blocks.BlockStopRain()
 
     val BLOCKS = arrayOf(
             blockTest,
